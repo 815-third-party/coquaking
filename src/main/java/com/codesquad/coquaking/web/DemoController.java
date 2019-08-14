ee517c048790fece1c845e74ccbf944d9cc7ea02
Ubuntu <ubuntu@ip-172-31-20-247.ap-northeast-2.compute.internal>
2019-08-14 23:59:56 +0000

    deploy: 1차 배포

--- src/main/java/com/codesquad/coquaking/web/DemoController.java
+++ src/main/java/com/codesquad/coquaking/web/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("POSTURL")
+@RequestMapping("/demo")
 public class DemoController {
     @Autowired
     private UserRepository userRepository;
