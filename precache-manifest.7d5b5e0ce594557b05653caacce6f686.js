13e5771a5b340171d83ae74bb847c70a89ac831d
leehwarang <hwrng2@gmail.com>
2019-08-15 16:29:15 +0900

    Updates

---
+++ precache-manifest.7d5b5e0ce594557b05653caacce6f686.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb05beb0709e6cbc8aef2821b102c00",
+    "url": "/coquaking/index.html"
+  },
+  {
+    "revision": "4d07ff10b9d913a2dcb3",
+    "url": "/coquaking/static/css/main.70d1dcea.chunk.css"
+  },
+  {
+    "revision": "64967fc865060981561e",
+    "url": "/coquaking/static/js/2.92a33bd6.chunk.js"
+  },
+  {
+    "revision": "4d07ff10b9d913a2dcb3",
+    "url": "/coquaking/static/js/main.80d5b476.chunk.js"
+  },
+  {
+    "revision": "69d8d15934d74d5de62a",
+    "url": "/coquaking/static/js/runtime~main.67cd5eaa.js"
+  }
+]);
